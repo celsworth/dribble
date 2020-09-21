d305884ad2d60147eefa5876a4b1ad49d53a32f2
Chris Elsworth <chris@cae.me.uk>
2020-09-21 21:05:51 +0100

    remove unused imports

--- frontend/src/Init.elm
+++ frontend/src/Init.elm
@@ -4,9 +4,6 @@ import Coders.Config
 import Dict
 import Json.Decode as JD
 import Model exposing (..)
-import Model.Utils.TorrentAttribute
-import Subscriptions
-import Utils.Filesize
 
 
 init : JD.Value -> ( Model, Cmd Msg )
