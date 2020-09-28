004084698d1abfef7da0f040e5eada12245612cd
Chris Elsworth <chris@cae.me.uk>
2020-09-28 16:42:03 +0100

    More rtorrent UNIX socket stuff

--- app.rb
+++ app.rb
@@ -5,10 +5,23 @@ require 'sassc'
 
 require 'rtorrent'
 
+# Connects to rtorrent at localhost:5000 by default. eg in rtorrent:
+#    network.scgi.open_port = 127.0.0.1:5000
+#
+# With the default docker-compose which sets ENV['DOCKER'] = true,
+# assumes rtorrent is running on the host at port 5000.
+#
+# To change rtorrent's location, set ENV['RTORRENT'] to the IP or a UNIX path.
+# To change the port from 5000, set ENV['RTORRENT_PORT'] (ignored for UNIX paths)
+
 class Dribble < Sinatra::Application
   configure do
     set :sockets, {}
-    set :rtorrent_host, ENV['DOCKER'] ? 'docker-host' : 'localhost'
+
+    # use the environment variable verbatim if set,
+    # else assume localhost, but cope with running in Docker
+    set :rtorrent_host, ENV['RTORRENT'] || (ENV['DOCKER'] ? 'docker-host' : 'localhost')
+    set :rtorrent_port, ENV['RTORRENT_PORT'] || 5000
 
     set :sass, style: :compact
 
@@ -28,7 +41,7 @@ class Dribble < Sinatra::Application
   # For the saved command and diffing stuff to work, d.hash must be the first arg.
   #
   def rtorrent_cmd(input, store:)
-    rtorrent = Rtorrent.new(settings.rtorrent_host, 5000)
+    rtorrent = Rtorrent.new(settings.rtorrent_host, settings.rtorrent_port)
 
     key = input['load'] || input['save']
     command = input['command'] || (key && store[key][:command])
