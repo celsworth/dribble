86ffe38da53b690da787433ed85f1e9bc24b3f53
Chris Elsworth <chris@cae.me.uk>
2020-10-06 20:49:50 +0100

    Use elm/parser to support ExactMatch and Regex in TorrentFilter

--- frontend/src/Model/TorrentFilter.elm
+++ frontend/src/Model/TorrentFilter.elm
@@ -1,13 +1,19 @@
 module Model.TorrentFilter exposing (..)
 
 import Model.Torrent exposing (Torrent)
+import Parser exposing ((|.), (|=), Parser, Step(..))
 import Regex exposing (Regex)
 
 
 type NameFilter
-    = Regex Regex
-    | Error
+    = Filters (List NameFilterComponent)
     | Unset
+
+
+type NameFilterComponent
+    = Regex Regex
+    | ExactMatch String
+    | Error
 
 
 type alias TorrentFilter =
@@ -20,37 +26,85 @@ setName new filter =
     { filter | name = parseName new }
 
 
-torrentMatches : TorrentFilter -> Torrent -> Bool
-torrentMatches filter torrent =
-    {- return true if it matches the given filter -}
+torrentMatches : Torrent -> TorrentFilter -> Bool
+torrentMatches torrent filter =
     case filter.name of
-        Regex re ->
-            Regex.contains re torrent.name
-
-        Error ->
-            False
+        Filters filters ->
+            List.all (torrentMatches2 torrent) filters
 
         Unset ->
             True
 
 
+torrentMatches2 : Torrent -> NameFilterComponent -> Bool
+torrentMatches2 torrent filter =
+    case filter of
+        Regex re ->
+            Regex.contains re torrent.name
+
+        ExactMatch str ->
+            String.contains (String.toLower str) (String.toLower torrent.name)
+
+        Error ->
+            False
+
+
 parseName : String -> NameFilter
 parseName input =
-    if String.isEmpty input then
-        Unset
+    case Parser.run parseNameFilter input of
+        Ok nameFilter ->
+            nameFilter
 
-    else
-        {- this does a lot.
-           * remove left/right whitespace
-           * split on spaces
-           * make those into (?=.*<foo>)
-           * join those together
-           * make a Regex; if it fails make an Error
-        -}
-        String.trim input
-            |> String.split " "
-            |> List.map (\s -> "(?=.*" ++ s ++ ")")
-            |> String.concat
-            |> Regex.fromStringWith { caseInsensitive = True, multiline = False }
-            |> Maybe.map Regex
-            |> Maybe.withDefault Error
+        Err _ ->
+            Unset
+
+
+parseNameFilter : Parser NameFilter
+parseNameFilter =
+    Parser.map Filters <|
+        Parser.loop [] parseNameFilterComponents
+
+
+parseNameFilterComponents : List NameFilterComponent -> Parser (Step (List NameFilterComponent) (List NameFilterComponent))
+parseNameFilterComponents filters =
+    Parser.oneOf
+        [ Parser.succeed ()
+            |. Parser.end
+            |> Parser.map (\_ -> Parser.Done filters)
+        , Parser.succeed (\filter -> Parser.Loop (filter :: filters))
+            |= parseNameFilterComponent
+            |. Parser.spaces
+        ]
+
+
+parseNameFilterComponent : Parser NameFilterComponent
+parseNameFilterComponent =
+    Parser.oneOf [ parseExactMatch, parseRegex ]
+
+
+parseExactMatch : Parser NameFilterComponent
+parseExactMatch =
+    Parser.succeed ExactMatch
+        |. Parser.symbol "\""
+        |= (Parser.getChompedString <|
+                Parser.succeed ()
+                    |. Parser.chompUntil "\""
+           )
+        |. Parser.symbol "\""
+
+
+parseRegex : Parser NameFilterComponent
+parseRegex =
+    Parser.succeed convertStringToRegex
+        |= (Parser.getChompedString <|
+                Parser.succeed ()
+                    |. Parser.chompUntilEndOr " "
+           )
+
+
+convertStringToRegex : String -> NameFilterComponent
+convertStringToRegex str =
+    str
+        |> Regex.fromStringWith { caseInsensitive = True, multiline = False }
+        |> Maybe.map Regex
+        |> Maybe.withDefault Error
