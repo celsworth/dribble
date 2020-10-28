e5593898b80242f6665a3aeeff2eacd9377abefb
Chris Elsworth <chris@cae.me.uk>
2020-10-28 09:19:34 +0000

    Rubocop

--- lib/xmlrpc/rtorrent.rb
+++ lib/xmlrpc/rtorrent.rb
@@ -2,22 +2,24 @@
 
 require 'xmlrpc/client'
 
-class XMLRPC::RTorrent < XMLRPC::Client
-  def do_rpc(xml, _async = false)
-    headers = { 'CONTENT_LENGTH' => xml.size, 'SCGI' => 1 }
+module XMLRPC
+  class RTorrent < XMLRPC::Client
+    def do_rpc(xml, _async = false)
+      headers = { 'CONTENT_LENGTH' => xml.size, 'SCGI' => 1 }
 
-    header = headers.to_a.flatten.join("\x00").to_s
-    request = "#{header.size}:#{header},#{xml}"
+      header = headers.to_a.flatten.join("\x00").to_s
+      request = "#{header.size}:#{header},#{xml}"
 
-    if @host.start_with?('/') || @host.start_with?('.')
-      UNIXSocket.open(@host) { |s| do_request(s, request) }
-    else
-      TCPSocket.open(@host, @port) { |s| do_request(s, request) }
+      if @host.start_with?('/') || @host.start_with?('.')
+        UNIXSocket.open(@host) { |s| do_request(s, request) }
+      else
+        TCPSocket.open(@host, @port) { |s| do_request(s, request) }
+      end
     end
-  end
 
-  def do_request(socket, request)
-    socket.write(request)
-    socket.read.split(/\n\s*?\n/, 2)[1]
+    def do_request(socket, request)
+      socket.write(request)
+      socket.read.split(/\n\s*?\n/, 2)[1]
+    end
   end
 end
